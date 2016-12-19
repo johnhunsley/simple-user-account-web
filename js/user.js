eedff3941a1e18f4cc225720d00696a2cc1c2ea7
johnhunsley <snowball33>
2016-12-19 13:28:36 +0000

    paging and filtering for users

--- js/user.js
+++ js/user.js
@@ -8,34 +8,44 @@ new Vue({
         nextPageNumber: 1,
         hasNext:true,
         previousPageNumber: 1,
-        hasPrevious: false
+        hasPrevious: false,
+        filter:""
     },
     mounted: function() {
-        this.getItems(10,1);
+        this.searchItems(10,1);
     },
     methods:{
         getItems: function(pageSize, pageNumber) {
             this.$http.get('http://localhost:8080/user/page/'+pageSize+"/"+pageNumber)
-                .then(function(data) {
-                    this.items = data.body.pagedItems;
-                    this.totalItems = data.body.totalItems;
-                    this.totalPages = data.body.totalPages;
-
-                    if(pageNumber < this.totalPages) {
-                        this.nextPageNumber = pageNumber +1;
-                        this.hasNext = true;
-                    } else {
-                        this.hasNext = false;
-                    }
-
-                    if(pageNumber > 1) {
-                        this.previousPageNumber = pageNumber -1;
-                        this.hasPrevious = true;
-                    } else {
-                        this.hasPrevious = false;
-                    }
-
+                .then(function(data){
+                    this.calculatePage(data, pageNumber);
                 });
+        },
+        searchItems: function(pageSize, pageNumber) {
+            this.$http.get('http://localhost:8080/user/search/'+pageSize+"/"+pageNumber+"?query="+this.filter)
+                .then(function(data){
+                    this.calculatePage(data, pageNumber);
+                });
+        },
+
+        calculatePage: function(data, pageNumber) {
+            this.items = data.body.pagedItems;
+            this.totalItems = data.body.totalItems;
+            this.totalPages = data.body.totalPages;
+
+            if(pageNumber < this.totalPages) {
+                this.nextPageNumber = pageNumber +1;
+                this.hasNext = true;
+            } else {
+                this.hasNext = false;
+            }
+
+            if(pageNumber > 1) {
+                this.previousPageNumber = pageNumber -1;
+                this.hasPrevious = true;
+            } else {
+                this.hasPrevious = false;
+            }
         }
     }
 });
